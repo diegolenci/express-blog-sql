151023cf12f7d1f51544f16526e61df63fe28b6b
diegolenci <lencidiego0@gmail.com>
2025-03-13 15:22:11 +0100

    copiato express-api-crud

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const cors = require('cors')
+
+const postRouter = require('./routers/postsRouter');
+
+//middlewares
+const notFound = require('./middlewares/notFound');
+const errorsHandler = require('./middlewares/errorsHandler');
+
+app.use(cors(
+    {
+        origin: 'http://localhost:5173'
+    }
+))
+
+app.use(express.static('public'));
+
+app.use(express.json());
+
+//home 
+// app.get('/', (req, res) => {
+//     res.send('Hello World!');
+// });
+
+app.use('/api/posts', postRouter);
+
+app.use(errorsHandler);
+app.use(notFound);
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
